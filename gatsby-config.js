c00cc0da14af18fdcab3efb7db401f496392a59f
Scott Motte <mot@mot.la>
2023-07-29 10:28:13 -0700

    Add .env.vault

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+require('dotenv').config()
+console.log(process.env) // for debugging purposes. remove when ready.
+
 module.exports = {
   siteMetadata: {
     title: `My Gatsby Site`,
